edc8a67c49064fda39cceab5645b345330c26bcb
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-20 18:37:00 -0800

    PythonPrep

---
+++ PythonPrep/is_leap.py
@@ -0,0 +1,15 @@
+'''
+Given a year, determine whether it is a leap year.
+    If it is a leap year, return the Boolean True,
+    otherwise return False.
+
+'''
+
+def is_leap(year):
+    leap = False
+
+    if (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0):
+        leap = True
+
+    return leap
+
