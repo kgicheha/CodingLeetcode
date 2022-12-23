58158975028a98231a37507d0428b84dc5ec9399
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-22 18:55:53 -0800

    JavaPrep

---
+++ JavaPrep/Arithmetic.java
@@ -0,0 +1,9 @@
+public class Arithmetic{
+
+    public static void main(String[] args) {
+        double result =  (double)10 % (double)3;
+        double result2 =  10 % 3;
+        System.out.println("You result is: " + result);
+        System.out.println("You result is: " + result2);
+    }
+}
