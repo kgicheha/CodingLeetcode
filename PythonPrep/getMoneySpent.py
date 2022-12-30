94ae3a09e4fe32a352b9e8a023c87ffa6480d1dd
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-30 13:31:41 -0800

    pythonPrep

--- PythonPrep/getMoneySpent.py
+++ PythonPrep/getMoneySpent.py
@@ -19,4 +19,35 @@ or
 return 58 because its the more expensive option
 '''
 
-def getMoneySpent(keyboard, drives, b):
+'''
+GIVEN: array of keyboard prices, array of USB drive prices, and a target price
+STEPS:
+    create empty object to store the differences
+    iterate first array
+        if the current value is <= target bugdet
+            get the difference between the current number in the keyboard array and target price
+            store the difference in the empty object
+    iterate second array
+        if the current value is
+
+RETURN: the sum of the two options that closest to the target price budget
+'''
+
+
+def getMoneySpent(keyboards, drives, b):
+    # BRUTE FORCE
+    max_spent = 0
+
+    for i in range(len(keyboards)):
+        for j in range(len(drives)):
+            cost = keyboards[i] + drives[j]
+            if (cost <= b):
+                if (max_spent == 0):
+                    max_spent = cost
+                elif (cost > max_spent):
+                    max_spent = cost
+
+    if (max_spent == 0):
+        print(-1)
+    else:
+        print(max_spent)
