569f0f11f4e1a7163b4ea8d0e3a4e46821dc7e45
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-31 10:08:44 -0800

    pythonPrep

--- PythonPrep/climbLeaderBoard.py
+++ PythonPrep/climbLeaderBoard.py
@@ -14,4 +14,42 @@ ranked = [100,90,90,80]
 player = [70,80,105]
 '''
 
+'''
+GIVEN: two arrays: ranked and player scores
+STEPS:
+    create a SET of ranked scores
+        This elimates duplicates from given ranked score array
+    sort the SET of the ranked scores
+        in reverse order
+
+    iterate player score array
+        if the score is greater than the first number in the ranked score
+            add a 1 to the results array
+                Meaning the player is in first place
+        else if the score is less than the last number in the ranked score
+            add [the length of SET of ranked scores + 1] to the results array
+RETURN: rank of player scores rank in an array
+'''
+
+
 def climbingLeaderboard(ranked, player):
+
+
+
+    # Sol3
+   result = []
+   unique_scores = sorted(set(ranked), reverse=True)
+   print(unique_scores)
+
+   for score in range(len(player)):
+    if(score >= unique_scores[0]):
+        result.push(1)
+    elif (score < unique_scores[len(unique_scores) - 1]):
+        result.push(len(unique_scores) +1)
+
+
+    print(result)
+ranked = [100,90,90,80]
+player = [70,80,105]
+
+climbingLeaderboard(ranked, player)
