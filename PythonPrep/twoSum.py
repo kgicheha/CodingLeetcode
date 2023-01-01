50b537181344fc4da61dd74ddb92dfeb1c6c0ab2
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-01 13:20:36 -0800

    pythonPrep

--- PythonPrep/twoSum.py
+++ PythonPrep/twoSum.py
@@ -24,9 +24,32 @@ Output: [1,2]
 '''
 GIVEN: array of numbers, target number
 STEPS:
+    create an hashMap
+       will store the current number and its index
 
+    loop through array
+        get the difference between the current number and the target value
+
+        if the difference is in the hashMap
+            return [index of the difference, current index]
+        else:
+            store the current number to the HashMap with its index as its value
 RETURN array of the indeces that sum up to given target number
 '''
 
 
 def twSum(nums, target):
+    prevMap = {} # value: index
+
+    for i, n in enumerate(nums):
+        diff = target - n
+
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return
+
+
+nums = [3,2,4]
+target = 6
+print(twSum(nums, target))
