9af96031f0ebf729a69f766bf091224431927f8e
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-01 12:54:52 -0800

    pythonPrep

--- PythonPrep/validAnagram.py
+++ PythonPrep/validAnagram.py
@@ -22,8 +22,54 @@ Output: false
 '''
 GIVEN: string
 STEPS:
-RESULT: return true if anaram, else return false
+    BASE CASE:
+        if the lengths are not the same,
+            return False
+
+    create two empty dictionaries
+        these will store the frequency count of the characters in both the given strings
+    iterate strings
+        if character is not in dict,
+            add it and set its count to 1
+        if it is in dict
+            increase count by 1
+    compare the count of the characters in the dictionary
+        if the count are not the same,
+            return false
+    return True
+        means that the string have passed the tests
+RESULT: return true if s is an anagram of t, else return false
 
 '''
 
-def isAnagram(s):
+def isAnagram(s,t):
+    # SOLUTION 1: using hasMap
+        # TIME and SPACE Complexities: O(n)
+            # have to iterate and save the characters in the strings
+
+    # if len(s) != len(t):
+    #     return False
+
+    # countS, countT = {}, {}
+
+    # for i in range(len(s)):
+    #     countS[s[i]] = 1 + countS.get(s[i], 0)
+    #     countT[t[i]] = 1 + countT.get(t[i], 0)
+
+    # for c in countS:
+    #     if countS[c] != countT.get(c, 0):
+    #         return False
+
+    # return True
+
+
+    # SOLUTION 2: sort characters
+        # TIME COMPLEXITY: O(n)
+        # SPACE COMPLEXITY: O(1)
+    return sorted(s) == sorted(t)
+
+
+s = "anagram"
+t = "nagaram"
+print(isAnagram(s,t))
+
