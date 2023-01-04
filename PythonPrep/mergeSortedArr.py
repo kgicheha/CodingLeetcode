33186344a8be8e3a02d557b7e3e63a0f4e4e33f1
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-03 16:58:32 -0800

    more leetcode practice

--- PythonPrep/mergeSortedArr.py
+++ PythonPrep/mergeSortedArr.py
@@ -32,13 +32,48 @@ GIVEN:
     two integer arays that are NOT SORTED
     two integers that represent the number of elements in nums1 and num2, respectively
 STEPS:
+    create pointer to point to the m + n -1 in nums1 array aft
+        meaning where the last value will be when both arrays are merged
 
-RETURN: dont return anything
+    loop array while the lengths of both arrays are more than 0
+
+        if the current value in nums1 is greater than the current value in num2 array
+            set the value of the pointer to equal nums1 value
+            decrement m by 1
+            decrement pointer by 1
+        else
+            set the value of the pointer equal to nums2 value
+            decrement n by 1
+            decrement pointer by 1
+
+        edge case: when the pointer reaches the first element of the nums1 array, however there are still values left in the nums 2 array:
+            set the value of the pointer equal to the last value in the nums2 array
+            decrement n by 1
+            decrement pointer by 1
+
+RETURN: dont return anything, merge nums1 as a sorted array
+
+
+TIME Complex: O(n)
+SPACE Complex: O(1)
 '''
 
+
 def merge(nums1, m, nums2, n):
+    last = (m + n) - 1
+   # merge in reverse order
+    while m > 0 and n > 0:
+        if nums1[m - 1] > nums2[n - 1]:
+            nums1[last] = nums1[m - 1]
+            m -= 1
+        else:
+            nums1[last] = nums2[n - 1]
+            n -= 1
 
-    while nums2:
-        nums1.push(nums2.pop())
+        last -= 1
 
-    nums1.sort()
+    # fill nums1 with left over elements in nums2
+    while n > 0:
+        nums1[last] = nums2[n - 1]
+        n -= 1
+        last -= 1
