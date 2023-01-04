1d4c62bf71ebb96f67adb73d413bdf6136a10d5d
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-04 10:44:15 -0800

    implementing graphs

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,4 +1,24 @@
-
 class Graph {
+  // creating new graph
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  // adding vertex or node
+  // if there isn't a vertex in the list with this value
+    // add it to the list
+    // set its value equal to an empty array
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true
+    }
+    return false
+  }
+
+//   adding edges
 
 }
+
+let myGraph = new Graph()
+myGraph.addVertex('A')
