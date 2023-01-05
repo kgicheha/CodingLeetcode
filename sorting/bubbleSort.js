d2ca623be7afe753197436675692a9747d1ab2f4
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-05 09:48:47 -0800

    implementing sorting

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,9 +1,14 @@
 /*
-will have a for loop inside of another for loop
-    first for loop
-        set i equal to the last index,
-        decrement i as you go
-    second for loop
+Bubble Sort
+Works by repeatedly swapping the adjacent elements if they are in the wrong order
+Not suitable for large data sets because its average and worst-case time complexity are quite high
+Time Complexity: O(N)^2
+Space Complexity: O(1)
+Process:
+	Starts with the very first two elements, comparing them to check which one is greater
+	If the element current element is greater than the right, you swap them
+	If the element current element is less than the right, you move to the next element
+
 
 */
 
---
+++ sorting/selectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  let min;
+  for (let i = 0; i < arr.length - 1; i++) {
+    min = i;
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr
+}
+
+console.log(selectionSort([4,2,6,5,1,3]))
