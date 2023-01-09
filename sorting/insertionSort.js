4fbbcf58e0b462373a70796d81fb88da224349d4
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-09 10:47:55 -0800

    implementing sorting

---
+++ sorting/insertionSort.js
@@ -0,0 +1,16 @@
+/*
+
+*/
+
+function instertionSort(arr){
+    let temp
+
+    for(let i = 1; i < arr.length; i++){
+        temp = arr[i]
+        for (var j = i - 1; arr[j] > temp && j > -1; j--){
+            arr[j + 1] = arr[j]
+        }
+        arr[j + 1] = temp
+    }
+    return arr
+}
