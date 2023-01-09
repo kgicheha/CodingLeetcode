9cf3acb87a17e0d37b9d608c1d8daa8e76a7642e
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-09 12:17:08 -0800

    implementing sorting

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,40 +1,47 @@
 /*
 uses recursion
+    1. breaks arrays in half
+    2. has base case: when array.length is 1
+        once it gets to the base case, thats when we use the merge function to put the array together
 */
 
 // merge Helper Function
-    // puts the array back together
-function merge(arr1, arr2){
-    let combined = []
-    let i = 0
-    let j = 0
+// puts the array back together
+function merge(arr1, arr2) {
+  let combined = [];
+  let i = 0;
+  let j = 0;
 
-    while(i < arr1.length && j < arr2.length){
-        if(arr1[i] < arr2[j]){
-            combined.push(arr1[i])
-            i++
-        } else {
-            combined.push(arr2[j])
-            j++
-        }
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      combined.push(arr1[i]);
+      i++;
+    } else {
+      combined.push(arr2[j]);
+      j++;
     }
+  }
 
-    while (i < arr1.length){
-        combined.push(arr1[i])
-        i++
-    }
-    while(j < arr2.length){
-        combined.push(arr2[j])
-        j++
-    }
+  while (i < arr1.length) {
+    combined.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    combined.push(arr2[j]);
+    j++;
+  }
 
-    return combined
+  return combined;
 }
 
-function mergeSort()
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
 
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
 
+  return merge(mergeSort(left), mergeSort(right));
+}
 
-console.log(merge([1,3,7,8], [2,4,5,6]))
-
-
+console.log(mergeSort([1, 3, 7, 8, 2, 4, 5, 6]));
