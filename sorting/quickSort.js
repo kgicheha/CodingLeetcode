39fdb598ae249e69cfb2dc85684fc551e911ecec
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-10 09:31:38 -0800

    implementing sorting

---
+++ sorting/quickSort.js
@@ -0,0 +1,29 @@
+function swap(arr, firstIndex, secondIndex) {
+  let temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+}
+
+function pivot(arr, pivotIndex = 0, endIndex = arr.length - 1) {
+  let swapIndex = pivotIndex;
+
+  for (let i = pivotIndex + 1; i <= endIndex; i++) {
+    if (arr[i] < arr[pivotIndex]) {
+      swapIndex++;
+      swap(arr, swapIndex, i);
+    }
+  }
+  swap(arr, pivotIndex, swapIndex);
+  return swapIndex;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr
+}
+
+console.log(quickSort([4, 2, 6, 5, 1, 3]));
