2a48976ba5ac9e6a42c0afbf5980b20085cf3d05
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-18 14:06:40 -0800

    leetcode practice

--- PythonPrep/minStartValue.py
+++ PythonPrep/minStartValue.py
@@ -27,12 +27,32 @@ startValue = 4 | startValue = 5 | nums
 '''
 '''
 GIVEN:
+    array of integers
 STEPS:
+    create sum variable and initialize it to 0
+    create result variable and initialize it to 0
+
+    loop through given array
+        the goal is to find the minimum sum value
+            then store it to the result variable
+
+    find the value that you would need to add to the minimum sum to get to 1
+        return 1 + the absolute value of the result
 RESULT:
+    minimum positive value
 '''
 
 def minStartValue(nums):
-    print(nums)
+    sum = 0
+    res = 0
 
-nums = [-3,2,-3,4,2]
+    for n in nums:
+        sum += n
+        res = min(res, sum)
+
+    return 1 + abs(res)
+
+
+
+nums = [1,-2,-3]
 print(minStartValue(nums))
