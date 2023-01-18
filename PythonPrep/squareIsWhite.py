6bf5d8b9f1bcbc1dff4e1f106587e218913b99c0
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-18 12:14:36 -0800

    leetcode practice

--- PythonPrep/squareIsWhite.py
+++ PythonPrep/squareIsWhite.py
@@ -15,12 +15,40 @@ the square with coordinates "a1" is black, so return false.
 
 '''
 GIVEN:
+    coordinates in a chessboard
 STEPS:
+    create a dictionary that separates the characters based on their index position
+        characters that have even number index belong to even key
+        characters that have odd number index belong to odd key
+
+    if the the first character in the given coordinate is in even key in that dictionary:
+        and the second character in the given coordinate is even
+            return TRUE
+        else return FALSE
+
+    else (if the first character in the given coordinate is in the odd key in the dictionary)
+        and the second character in the given coordinate is odd,
+            return TRUE
+        else:
+            return FALSE
 RESULT:
+    return TRUE is the given coordinate is white,
+    return FALSE if the given coordinate is black
 
 '''
 def squareIsWhite(coordinates):
-    print(coordinates)
+    letters ={'even':['a', 'c','e', 'g'], 'odd':['b', 'd', 'f', 'h']}
+
+    if coordinates[0] in letters['even']:
+        if int(coordinates[1]) % 2 == 0:
+            return True
+        else:
+            return False
+    else:
+        if int(coordinates[1]) % 2 != 0:
+            return True
+        else:
+            return False
 
 
 coordinates = "a1"
