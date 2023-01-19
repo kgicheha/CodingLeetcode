3377308408816a0902c41928a5d1a2697bcf7b27
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-18 19:01:21 -0800

    leetcode practice

--- PythonPrep/stringPalindrome.py
+++ PythonPrep/stringPalindrome.py
@@ -19,12 +19,47 @@ Explanation: "amanaplanacanalpanama" is a palindrome.
 '''
 GIVEN: string
 STEPS:
+    TWO POINTER SOLUTION
+    set left pointer equal to the first index
+    set the right pointer equal to the last index
+
+    while left pointer < right pointer:
+        while left pointer < right pointer and character at left pointer is not an alphabet or number:
+            increase left pointer by 1
+        while left pointer < right pointer and character at right pointer is not an alphabet or number:
+            decrement right pointer by 1
+
+        if the character at the left pointer is not equal to the character at the right pointer:
+            return False
+
+        else:
+            increment left pointer by 1
+            decrement right pointer by 1
+
+        return True
+
 RESULT: return true if string is a palindrome, else return false
 
 '''
 
 def isPalindrome(s):
-    print(s)
+    lp = 0
+    rp = len(s) - 1
+
+    while lp < rp:
+        while lp < rp and not s[lp].isalnum():
+            lp += 1
+        while lp < rp and not s[rp].isalnum():
+            rp += 1
+
+        if s[lp].lower() != s[rp].lower():
+            return False
+
+        lp +=1
+        rp -=1
+        return True
+
+
 
 s = "A man, a plan, a canal: Panama"
 print(isPalindrome(s))
