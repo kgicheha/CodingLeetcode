dfdd75824e5f82ab471101baabd472f08f9f8a28
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-18 19:13:25 -0800

    leetcode practice

--- PythonPrep/stringPalindrome.py
+++ PythonPrep/stringPalindrome.py
@@ -50,7 +50,7 @@ def isPalindrome(s):
         while lp < rp and not s[lp].isalnum():
             lp += 1
         while lp < rp and not s[rp].isalnum():
-            rp += 1
+            rp -= 1
 
         if s[lp].lower() != s[rp].lower():
             return False
