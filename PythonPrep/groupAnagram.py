88cd995db9317f0c9528ac52f0a2a467c7054fd3
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-18 19:44:57 -0800

    leetcode practice

--- PythonPrep/groupAnagram.py
+++ PythonPrep/groupAnagram.py
@@ -1,3 +1,5 @@
+
+from collections import defaultdict
 '''
 49. Group Anagrams
 Given an array of strings strs, group the anagrams together.
@@ -17,14 +19,47 @@ Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 '''
 
 '''
-GIVEN:
+GIVEN: array of strings
 STEPS:
-RETURN:
+    create empty dictionary
+    loop through given array
+        sort every word in the array
+        check to see if the sorted word is in the array
+
+        if sorted word is not dictionary, add it to the dictionary
+            the key will be the sorted word
+            the value will be the given word
+
+        if the sorted word is in the dictionary:
+                append the given word as the value of the sorted word
+
+        create empty output array
+        loop through the dictionary
+            append all the values to the output array
+RETURN: array of arry of groups anagrams
 
 '''
 
 def groupAnagrams(strs):
-    print(strs)
+
+    # lookup = defaultdict(list)
+    lookup = {}
+
+    for word in strs:
+            sorted_word = sorted(word)
+            key = "".join(sorted_word)
+
+            if key not in lookup:
+                lookup[key] = [word]
+            else:
+                lookup[key].append(word)
+
+    output = []
+    for item in lookup.values():
+        output.append(item)
+
+    return output
+
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 print(groupAnagrams(strs))
