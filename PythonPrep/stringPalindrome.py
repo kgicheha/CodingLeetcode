779d7f8c5703109b2c64db8af6a40c0fb90625e1
Kevin Gicheha <gichehakevin@gmail.com>
2023-01-18 18:46:16 -0800

    leetcode practice

---
+++ PythonPrep/stringPalindrome.py
@@ -0,0 +1,30 @@
+'''
+125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters
+into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters
+include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+'''
+
+'''
+EXAMPLE 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+'''
+
+'''
+GIVEN: string
+STEPS:
+RESULT: return true if string is a palindrome, else return false
+
+'''
+
+def isPalindrome(s):
+    print(s)
+
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
