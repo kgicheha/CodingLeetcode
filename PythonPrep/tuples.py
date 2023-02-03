eb6171e5e4ae0659da30c8810d589aac74cc1dfb
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-02 16:44:39 -0800

    leetcode prep

--- PythonPrep/tuples.py
+++ PythonPrep/tuples.py
@@ -1,2 +1,5 @@
 def tuples():
-    print(hash())
+    tuple_length = int(input())
+    raw_input = input().split(" ")
+    tuple_converter = tuple(map(int, raw_input))
+    print(hash(tuple_converter))
