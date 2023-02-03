f7f1ce89220c41155a9cc1eb6705e312643de5c0
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-02 17:54:41 -0800

    leetcode prep

--- PythonPrep/divisibleSumPairs.py
+++ PythonPrep/divisibleSumPairs.py
@@ -3,3 +3,15 @@ Given an array of integers and a positive integer k,
 determine the number of (i,j) pairs where i < j and ar[i] + ar[j]
 is divisible by .
 '''
+
+'''
+GIVEN:
+    1. array of integers
+    2. positive integer
+    3. length of array
+STEPS:
+RESULT:
+'''
+
+def divisibleSumPairs(n, k, ar):
+    print(n)
