03207c1b1980e9639037bdec1fb8a7a9f719f4d3
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-06 13:31:07 -0800

    leetcode problems

--- PythonPrep/closestNumbers.py
+++ PythonPrep/closestNumbers.py
@@ -1,9 +1,7 @@
 '''
-Sorting is useful as the first step in many different tasks.
- The most common task is to make finding things easier,
- but there are other uses as well. In this case,
- it will make it easier to determine which pair or pairs of elements
- have the smallest absolute difference between them.
+Given a list of unsorted integers, ,
+find the pair of elements that have the smallest absolute difference between them.
+If there are multiple pairs, find them all.
 '''
 
 def closestNumbers(arr):
