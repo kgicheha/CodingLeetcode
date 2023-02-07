16c36333386936cf51c6d6eb6c85544c25f08db2
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-06 16:58:32 -0800

    leetcode problems

--- PythonPrep/findMedian.py
+++ PythonPrep/findMedian.py
@@ -1,3 +1,5 @@
+import math
+
 '''
 Given a list of numbers with an odd number of elements, find the median?
     Median is the value separating the higher half from the lower half of a data sample
@@ -5,7 +7,17 @@ Given a list of numbers with an odd number of elements, find the median?
 '''
 
 def findMedian(arr):
-    print(arr)
+    arr.sort()
+    # SOLUTION 1
+    medium_index = (arr[0] + (len(arr)-1)) // 2
+    result = arr[medium_index]
+    print(result)
+
+    # SOLUTION 2
+    # median = arr[len(arr) // 2]
+
+    # return median
+
 
 arr =[5,3,1,2,4]
 findMedian(arr)
