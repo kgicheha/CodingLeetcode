bd338680eda9695804be40b355fb70eac82f3c36
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-08 11:49:54 -0800

    leetcode problems

--- PythonPrep/sherlockAndAnagrams.py
+++ PythonPrep/sherlockAndAnagrams.py
@@ -1,2 +1,17 @@
+'''
+Two strings are anagrams of each other if the letters of one string
+can be rearranged to form the other string.
+Given a string, find the number of pairs of substrings of the string
+that are anagrams of each other.
+'''
+
+'''
+GIVEN:
+
+STEPS:
+RESULT:
+    the number of unordered anagrammatic pairs of substrings in s
+'''
+
 def sherlockAndAnagrams(s):
     print(s)
