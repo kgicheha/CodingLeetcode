276a2209b66cf9968b1ebbbe69949a6af63d30c5
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-09 10:39:34 -0800

    algo practice

--- PythonPrep/largestsumSubarray.py
+++ PythonPrep/largestsumSubarray.py
@@ -8,12 +8,42 @@ GIVEN:
     array
 STEPS:
     KADANE'S ALGORITHM
+
+    initialize:
+        max_so_far = arr[0]
+        global_max = 0
+
+
+    loop for each element in the array
+        global_max = global_max + arr[i]
+
+        if global_max < 0:
+            global_max = 0
+
+        elif (max_so_far < global_max):
+            max_so_far = max_ending
+
+    return global_max
 RESULT:
     return the largest sum of the contigious subarray
 '''
 
 def largestsumSubarray(arr):
 
-    print(arr)
+    current_sum = 0
+    max_so_far = arr[0]
+
+    for i in range(len(arr)):
+        current_sum = current_sum + arr[i]
+
+        if current_sum < 0:
+            current_sum = 0
+
+        # elif (max_so_far < current_sum):
+        #     max_so_far = current_sum
+
+        max_so_far = max(max_so_far, current_sum)
+
+    print(max_so_far)
 
 largestsumSubarray([-2,-3,4,-1,-2,1,5,-3])
