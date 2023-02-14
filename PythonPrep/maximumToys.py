bfa4bf9f4cbb5efa15061c68e1d013c7bf538659
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-14 15:22:59 -0800

    practice practice

---
+++ PythonPrep/maximumToys.py
@@ -0,0 +1,12 @@
+'''
+Mark and Jane are very happy after having their first child.
+Their son loves toys, so Mark wants to buy some.
+There are a number of different toys lying in front of him,
+tagged with their prices. Mark has only a certain amount to spend,
+and he wants to maximize the number of toys he buys with this money.
+Given a list of toy prices and an amount to spend,
+determine the maximum number of gifts he can buy.
+'''
+
+def maximumToys(prices, k):
+    print(k)
