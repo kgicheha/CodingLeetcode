2c6e6f5ead71061978f255a1b65e75f7c9a77544
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-15 15:34:32 -0800

    practice practice

--- PythonPrep/whatFlavors.py
+++ PythonPrep/whatFlavors.py
@@ -17,9 +17,40 @@ Two ice creams having unique IDs i and j may have the same cost (i.e., cost[i] =
 '''
 
 '''
-cost = [2,1,3,5,6]
-money = 5
+GIVEN:
+    array of integers
+    money
+STEPS:
+    initilze empty object that will be used to store the difference between the current number and the given money
+
+    loop through the given arry and store the absolute diffnerce between the current number and money to the array
+
+RESULT
 '''
 
 def whatFlavors(cost, money):
-    print(cost)
+    diff ={}
+    semi_result =[]
+    result = []
+
+    for i in range(len(cost)):
+        cur_dif = (money - cost[i])
+
+        if cur_dif > 1:
+            diff[cost[i]] = cur_dif
+
+    print(diff)
+
+    for key in diff:
+        if diff[key] in cost:
+            semi_result.append(diff[key])
+
+    for i in range(len(semi_result)):
+        if semi_result[i] in cost:
+            result.append(i)
+
+    print(semi_result)
+    print(result)
+cost = [2,1,3,5,6]
+money = 5
+whatFlavors(cost, money)
