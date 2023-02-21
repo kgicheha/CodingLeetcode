0aabcdf11b9abc208988c1d8ed36c3ee7b8a8414
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-21 10:13:12 -0800

    practice

--- PythonPrep/subArrayDiv.py
+++ PythonPrep/subArrayDiv.py
@@ -16,6 +16,21 @@ s = [2,2,1,3,2]
 d = 4
 m = 2
 
+Lily wants to find segments summing to Ron's birth day, d = 4
+with a length equalling his birth month, m = 2.
+In this case, there are two segments meeting his criteria: [2,2] and [1,3]
+'''
+
+'''
+GIVEN:
+    s: represents an array
+    d: represents day of his birthday
+    m: represents month of his birthday
+STEPS:
+RESULT:
+    return array of CONTIGIOUS segement that fits:
+        the LENGTH of the segment matches bithday MONTH(m)
+        SUM of the integers is equal to bithDAY (d)
 '''
 def birthday(s,d,m):
     print(s)
