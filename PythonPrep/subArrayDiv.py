c721ca22b9c00d0c3e8bec7001f7c21c243f9eab
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-21 13:59:25 -0800

    practice

--- PythonPrep/subArrayDiv.py
+++ PythonPrep/subArrayDiv.py
@@ -27,14 +27,45 @@ GIVEN:
     d: represents day of his birthday
     m: represents month of his birthday
 STEPS:
-RESULT:
-    return array of CONTIGIOUS segement that fits:
-        the LENGTH of the segment matches bithday MONTH(m)
-        SUM of the integers is equal to bithDAY (d)
+    inialize left pointer equal to 0
+    initialize right pointers equal to m
+        difference between left pointer and M represents the size of the window
+
+    initialize total segment count to 0
+        this will keep count of the subarray that meet the criteria
+
+    While right pointer is less than or equal to the length of array:
+
+       get the sum of the sub elements from the left pointer and the right pointer
+
+        if(sum == d) :
+            increment total segment count by 1
+
+        increment left pointer by 1
+        increment right pointer by 1
+
+    return the total segment count
+
 '''
 def birthday(s,d,m):
-    print(s)
 
+    left = 0
+    right = m
+
+    total_seg_count = 0
+
+    while right <= len(s):
+
+        sumSubArry = sum(s[left: right])
+
+        if sumSubArry == d:
+            total_seg_count += 1
+
+        left += 1
+        right +=1
+
+
+    print(total_seg_count)
 
 s = [2,2,1,3,2]
 d = 4
