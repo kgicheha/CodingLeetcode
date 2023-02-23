d2efa2082ce1f5d621b188d89d51719582a41735
Kevin Gicheha <gichehakevin@gmail.com>
2023-02-23 08:50:24 -0800

    practice

---
+++ PythonPrep/fourSum.py
@@ -0,0 +1,14 @@
+'''
+Given an array nums of n integers,
+return an array of all the unique quadruplets [nums[a], nums[b], nums[c],
+nums[d]] such that:
+
+You may return the answer in any order.
+'''
+
+def fourSum(arr, target):
+    print(arr)
+
+arr = [1,0,-1,0,-2,2]
+target = 0
+fourSum(arr, target)
