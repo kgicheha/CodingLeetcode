e879ce3db4703c20d5746c16e2c519ba32a4a8b6
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-05 18:39:54 -0800

    practice

--- PythonPrep/isPerm.py
+++ PythonPrep/isPerm.py
@@ -10,7 +10,54 @@ are Permutation of each other.
 '''
 
 def isPermutation(str1, str2):
-    seen = {}
-    # for i in str1:
-    #     if
+
+    if len(str1) != len(str2):
+        return False
+
+
+    #SOLUTION 1: USING OBJECT
+    # TIME COMPLEXITY: O(n + m)
+    # SPACE COMPLEXITY: O(n)
+
+    # seen = {}
+
+    # for i in range(len(str1)):
+    #     if str1[i] not in seen:
+    #         seen[str1[i]] = 1
+    #     else:
+    #         seen[str1[i]] += 1
+
+    # print(seen)
+
+    # for i in range(len(str2)):
+    #     if str2[i] in seen:
+    #         seen[str2[i]] -= 1
+    #     else:
+    #         return False
+
+    # if sum(seen.values()) == 0:
+    #     return True
+    # else:
+    #     return False
+
+
+
+    # SOLUTION 2: WITHOUT USING OBJECT
+    # TIME COMPLEXITY = O(n)
+    # SPACE COMPLEXITY = O(1)
+
+    sorted_str1 = sorted(str1)
+    joined_str1 = "".join(sorted_str1)
+
+    sorted_str2 = sorted(str2)
+    joined_str2 = "".join(sorted_str2)
+
+    combined_str = sorted(joined_str1 + joined_str2)
+    print(combined_str)
+
+
+
+# print(isPermutation("test", "rest"))
+print(isPermutation("test", "test"))
+
 
