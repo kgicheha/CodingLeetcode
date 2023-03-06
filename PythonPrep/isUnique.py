144febd1e2220ddabeb137eeeb1362ec6647209c
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-05 17:55:09 -0800

    practice

--- PythonPrep/isUnique.py
+++ PythonPrep/isUnique.py
@@ -2,8 +2,31 @@
 is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
 cannot use additional data structures?
 '''
+
+
 def isUnique(str):
+
+    # USING ANOTHER DATA STRUCTURE
+    # seen = set()
+
+    # for s in str:
+    #     if s not in seen:
+    #         seen.add(s)
+    #     else:
+    #         return False
+
+    # return True
+
+
+    # WITHOUT USING ANOTHER DATA STRUCTURE
+
+    str.sort()
+
     print(str)
+
+
+
+
 
 
 isUnique("abcd10jk")
