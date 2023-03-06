03948a36998aa013a2c9f32171e9784a2c9f8b2f
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-05 18:08:58 -0800

    practice

---
+++ PythonPrep/isPerm.py
@@ -0,0 +1,16 @@
+'''
+Check Permutation:
+Given two strings, write a method to decide if one is a permutation of the
+other.
+
+Permutation of a string is another string that contains same characters,
+only the order of characters can be different. For example, “abcd” and “dabc”
+are Permutation of each other.
+
+'''
+
+def isPermutation(str1, str2):
+    seen = {}
+    # for i in str1:
+    #     if
+
