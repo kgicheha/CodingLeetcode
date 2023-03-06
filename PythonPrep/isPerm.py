a904c42f8d93c78d70fbf959c49e69f04b3f2f8a
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-05 18:52:42 -0800

    practice

--- PythonPrep/isPerm.py
+++ PythonPrep/isPerm.py
@@ -47,17 +47,15 @@ def isPermutation(str1, str2):
     # SPACE COMPLEXITY = O(1)
 
     sorted_str1 = sorted(str1)
-    joined_str1 = "".join(sorted_str1)
-
     sorted_str2 = sorted(str2)
-    joined_str2 = "".join(sorted_str2)
 
-    combined_str = sorted(joined_str1 + joined_str2)
-    print(combined_str)
+    if sorted_str1 == sorted_str2:
+        return True
+    else:
+        return False
 
 
-
-# print(isPermutation("test", "rest"))
+print(isPermutation("test", "rest"))
 print(isPermutation("test", "test"))
 
 
