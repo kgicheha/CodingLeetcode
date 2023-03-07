2658ed319368155dffb1ded80aba94b782806f06
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-07 14:07:24 -0800

    practice

--- PythonPrep/linkedList.py
+++ PythonPrep/linkedList.py
@@ -14,34 +14,34 @@ class LinkedList:
             print(current.data)
             current = current.next
 
-    def reverseList(self, head):
+    def reverseList(self):
         # if list is empty
 
-        if head is None:
-            return head
+        if self.head is None:
+            return self.head
 
-        current = head
+        current = self.head
         previous = None
         next = None
 
         while current != None:
+
             next = current.next
             current.next = previous
             previous = current
             current = next
+
         return previous
 
     def append(self, data):
-        # create new Node using the Node Class
+
         new_node = Node(data)
 
-        # if list is empty, set the head to new node
+
         if self.head == None:
             self.head = new_node
             return
 
-        # traverse node until you get to the last node
-        # set the last node.next equal to new node
         current = self.head
         while current.next != None:
             current = current.next
@@ -53,4 +53,6 @@ class LinkedList:
 my_llist = LinkedList()
 my_llist.append(5)
 my_llist.append(3)
+my_llist.append(1)
 my_llist.printList()
+my_llist.reverseList()
