c1c49e2ba6825c39557831e1305010a3a39516b0
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-07 18:29:23 -0800

    practice

--- PythonPrep/linkedList.py
+++ PythonPrep/linkedList.py
@@ -6,6 +6,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.length = 1
 
     def printList(self):
         current = self.head
@@ -29,6 +30,8 @@ class LinkedList:
 
             current.next = new_node
 
+            self.length += 1
+
     # ADDING AT THE BEGGING OF LIST
     def prepend(self,data):
         new_node = Node(data)
@@ -38,6 +41,8 @@ class LinkedList:
 
         new_node.next = self.head
         self.head = new_node
+
+        self.length += 1
 
     # ADDING INSIDE THE LIST
     def insert_after_node(self, previous_node, data):
@@ -51,15 +56,78 @@ class LinkedList:
         new_node.next = previous_node.next
         previous_node.next = new_node
 
+        self.length += 1
+
     # ADDING IN THE MIDDLE OF A LINKED LIST
     def insert_in_middle(self, data):
 
+        new_node = Node(data)
+
+        # get the length of list
         list_length = 0
         current = self.head
 
         while current:
             list_length += 1
             current = current.next
+
+        # print("Length of List",list_length)
+
+        # get the middle index then traverse to the middle
+        middle = list_length // 2
+
+        current = self.head
+
+        counter = 0
+        while current:
+            if counter == middle - 1 :
+                new_node.next = current.next
+                current.next = new_node
+
+            current = current.next
+            counter += 1
+
+        self.length += 1
+
+    # GET NODE FROM A PARTICULAR INDEX
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+
+        current = self.head
+
+        i = 0
+        while i < index:
+            current = current.next
+            i += 1
+        return current
+
+    def set(self, index, data):
+        current = self.get(index)
+
+        if current:
+            current.data = data
+            return True
+
+        return False
+
+    # DELETION AT THE END OF LIST
+    def pop(self):
+        if self.head == None:
+            return None
+
+        current = self.head
+        previous = self.head
+
+        while current.next:
+            previous = current
+            current = current.next
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+
+        return current.data
 
     def reverseList(self):
         # if list is empty
@@ -86,10 +154,17 @@ class LinkedList:
 
 
 my_llist = LinkedList()
-print(my_llist.append(5))
-my_llist.append(3)
+my_llist.append(1)
 my_llist.append(2)
+my_llist.append(3)
+my_llist.append(4)
 # my_llist.prepend(1)
-my_llist.insert_after_node(my_llist.head.next, 1)
+# my_llist.insert_after_node(my_llist.head.next, 1)
+# my_llist.insert_in_middle(5)
+my_llist.printList()
+# print(my_llist.get(2))
+# print(my_llist.set(0, 109))
+# my_llist.printList()
+print(my_llist.pop())
 my_llist.printList()
 # my_llist.reverseList()
