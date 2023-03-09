729697f77d7481497a6af6dc4fc474fda640f1e8
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-09 08:54:03 -0800

    practice

--- PythonPrep/binary_tree.py
+++ PythonPrep/binary_tree.py
@@ -1,37 +1,41 @@
-class treeNode:
+class Node:
     def __init__(self, data):
         self.data = data
         self.leftChild = None
         self.rightChild = None
 
+
+class binaryTree:
+    def createNode(self, data):
+        return Node(data)
+
     # ADDING CHILDREN
-    def insert(self, data):
+    def insert(self,node, data):
 
-        if data < self.data:
-            if self.leftChild:
-               self.leftChild.insert(data)
-            else:
-                self.leftChild = treeNode(data)
-                return
+        # if the tree is empty, create new Node
+        if node is None:
+            return self.createNode(data)
+
+        # if the given data is less than the current node data
+        if data < node.data:
+            node.leftChild = self.insert(node.leftChild, data)
+
+        # else if given data is more than the current node data
         else:
-            if self.rightChild:
-                self.rightChild.insert(data)
-            else:
-                self.rightChild = treeNode(data)
-                return
+            node.rightChild = self.insert(node.rightChild, data)
 
+        return node
 
-    def printInorder(self):
+    def printInorder(self, root):
 
-            # self.printInorder(self.leftChild)
-        if self.leftChild:
-            self.leftChild.printInorder()
+        result = []
 
-        print(self.data)
+        if root is not None:
+            self.printInorder(root.leftChild)
+            result.append(self.data)
+            self.printInorder(root.rightChild)
 
-            # self.printInorder(self.rightChild)
-        if self.rightChild:
-            self.rightChild.printInorder()
+        print(result)
 
     def printPreOrder(self):
 
@@ -53,9 +57,11 @@ class treeNode:
 
         print(self.data)
 
-bin_tree = treeNode(1)
-bin_tree.insert(2)
-bin_tree.insert(3)
-bin_tree.insert(4)
-bin_tree.insert(5)
-bin_tree.printInorder()
+
+bin_tree = binaryTree()
+root = bin_tree.createNode(1)
+bin_tree.insert(root, 2)
+bin_tree.insert(root, 3)
+bin_tree.insert(root, 4)
+bin_tree.insert(root, 5)
+# bin_tree.printInorder()
