09b8547887cf485c446b97000803c75a911305f5
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-15 09:51:09 -0700

    practice

---
+++ PythonPrep/bubbleSort.py
@@ -0,0 +1,9 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+arr = [3,2,1,5,7]
+print(bubbleSort(arr))
