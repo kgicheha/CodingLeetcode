2b295f40f5f222d827e593bec56b9f86536ddeaa
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-28 09:51:46 -0700

    practice

--- PythonPrep/mergeSort.py
+++ PythonPrep/mergeSort.py
@@ -13,5 +13,34 @@ def mergeSort(arr):
     print(arr)
 
 
+
+
+
+def merge_two_sorted_lists(a,b):
+    sorted_list = []
+
+    len_a = len(a)
+    len_b = len(b)
+
+
+    i = j = 0
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            sorted_list.append(a[j])
+            i += 1
+        else:
+            sorted_list.append(b[j])
+            j += 1
+
+    while i < len_a:
+        sorted_list.append(a[i])
+        i += 1
+    while j < len_b:
+        sorted_list.append(b[j])
+        j += 1
+
+
+    return sorted_list
+
 arr = [3,2,6,7,1,2,4]
 mergeSort(arr)
