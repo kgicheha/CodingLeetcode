f215354868a4b6e74ee3112593ac2f33f8717ed2
Kevin Gicheha <gichehakevin@gmail.com>
2023-03-29 09:24:20 -0700

    practice

---
+++ PythonPrep/firstMissingPositive.py
@@ -0,0 +1,24 @@
+'''
+41. First Missing Positive
+
+Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+'''
+
+'''
+EXAMPLE 1:
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+
+EXAMPLE 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+'''
+
+def firstMissingPositive(nums):
+    print(nums)
+
