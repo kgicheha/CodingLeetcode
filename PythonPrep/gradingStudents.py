4e1348bbf87c5c3d86f3fc78776135922d035fab
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-03 21:30:12 -0700

    practice

--- PythonPrep/gradingStudents.py
+++ PythonPrep/gradingStudents.py
@@ -1,3 +1,14 @@
+'''
+HackerLand University has the following grading policy:
+
+    Every student receives a grade in the inclusive range from 0 to 100.
+    Any  less than  is a failing grade.
+
+
+
+'''
+
+
 def gradingStudents(grades):
     print(grades)
 
