bdca91f0500adf879a3cc2d5786a8abf7daeaa5d
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-11 14:40:52 -0700

    practice

--- PythonPrep/algoExpert/twoNumberSum.py
+++ PythonPrep/algoExpert/twoNumberSum.py
@@ -5,13 +5,30 @@ https://www.algoexpert.io/questions/two-number-sum
 
 '''
 GIVEN:
+    1. non-empty array of distinct integers
+    2. target sum
 STEPS:
-RESULT
+RESULT:
 '''
 
 def twoNumberSum(array, targetSum):
-    # Write your code here.
-    print(array)
+    # SOLUTION 1: USING AN OBJECT
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+    seen = {}
+
+    for value in array:
+        dif = targetSum - value
+
+        if dif not in seen:
+            seen[value] = dif
+        else:
+            result = [seen[dif], dif]
+            return result
+    return []
+
+
+    # SOLUTION 2:
 
 array = [3,5,-4,8,11,1,-1,6]
 targetSum = 10
