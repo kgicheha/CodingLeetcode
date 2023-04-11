9af5b7f0e56ef5d100012da0d664c5f4c9b96235
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-11 08:30:06 -0700

    practice

--- PythonPrep/isValidSubsequence.py
+++ PythonPrep/isValidSubsequence.py
@@ -15,14 +15,17 @@ SPACE COMPLEXITY: O(1) --> we dont have to create any new data structure
 '''
 
 def isValidSubsequence(array, sequence):
-    lp = 0
-    rp = 0
+    arrIndex = 0
+    seqIndex = 0
 
-    while lp <= len(sequence):
-        if sequence[lp] in array:
-            lp += 1
-    return True
+    while arrIndex < len(array) and seqIndex < len(sequence):
+
+        if array[arrIndex] == sequence[seqIndex]:
+            seqIndex += 1
+
+        arrIndex += 1
+    return seqIndex == len(sequence)
 
 array = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [5, 1, 22, 25, 6, -1, 8, 10]
-isValidSubsequence(array, sequence)
+print(isValidSubsequence(array, sequence))
