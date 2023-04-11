22dc970e15827981c8c2aefee7481e5af4fc448f
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-11 08:20:20 -0700

    practice

--- PythonPrep/isValidSubsequence.py
+++ PythonPrep/isValidSubsequence.py
@@ -2,10 +2,23 @@
 Question:
 https://www.algoexpert.io/questions/validate-subsequence
 '''
-
+'''
+GIVEN:
+    2 non-empty arrays
+STEPS:
+RETURN
+    return True, if the sequence is in the first array
+    else return False
+'''
 
 def isValidSubsequence(array, sequence):
-    print(array)
+    lp = 0
+    rp = 0
+
+    while lp <= len(sequence):
+        if sequence[lp] in array:
+            lp += 1
+    return True
 
 array = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [5, 1, 22, 25, 6, -1, 8, 10]
