ce0618a59a5b3359ab6fa7711f0ec5acc7f4e5f7
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-12 10:10:03 -0700

    practice

--- PythonPrep/algoExpert/tournamentWinner.py
+++ PythonPrep/algoExpert/tournamentWinner.py
@@ -4,13 +4,49 @@ https://www.algoexpert.io/questions/tournament-winner
 '''
 '''
 GIVEN:
+    1. array of pairs representing the teams tht have competed against each other
+    2. array containing the results of each competition
 STEPS:
-RESULTS:
+    initialize a empty dictionary to keep track of scores
 
+    traverse given competitions array:
+        homeTeam will be the first element in each sub array
+        awayTeam will be the second element in each sub array
+
+        if the results of the game is 1:
+            this means that the
+
+RESULTS:
+    return winner of the tournament
 '''
 
 def tournamentWinner(competitions, results):
-    print(results)
+
+    scoreCount = {}
+
+    for currentGame in range(len(competitions)):
+        homeTeam = competitions[currentGame][0]
+        awayTeam = competitions[currentGame][1]
+        print("HomeTeam",homeTeam)
+        print("AwayTeam",awayTeam)
+
+
+        if results[currentGame] == 1:
+            if competitions[currentGame][0] not in scoreCount:
+                scoreCount[competitions[currentGame][0]] = 1
+            else:
+                scoreCount[competitions[currentGame][0]] += 1
+        elif results[currentGame] == 0:
+            if competitions[currentGame][1] not in scoreCount:
+                scoreCount[competitions[currentGame][1]] = 1
+            else:
+                scoreCount[competitions[currentGame][1]] += 1
+
+    print(scoreCount)
+
+    max_key = max(scoreCount, key= scoreCount.get)
+
+    return max_key
 
 
 
