85d18b32f3c610014d3d651c958b4c26cbf40b44
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-13 16:34:00 -0700

    practice

---
+++ PythonPrep/algoExpert/findClosestValueInBst.py
@@ -0,0 +1,21 @@
+'''
+Question Link:
+https://www.algoexpert.io/questions/find-closest-value-in-bst
+'''
+
+'''
+GIVEN:
+STEPS:
+RESULT:
+
+'''
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def findClosestValueInBst(tree, target):
+    print(tree)
+
