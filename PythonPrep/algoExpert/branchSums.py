2531b4c7f571e30c1c42facf8d2441dae1cc92eb
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-18 09:34:23 -0700

    practice

---
+++ PythonPrep/algoExpert/branchSums.py
@@ -0,0 +1,16 @@
+
+'''
+Question Link:
+https://www.algoexpert.io/questions/branch-sums
+
+'''
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def branchSums(root):
+    print(root)
+
