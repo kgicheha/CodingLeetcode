8c7fa3feb106a843b8a1e1a7d3a77eb013fd8c23
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-17 17:54:27 -0700

    practice

--- PythonPrep/algoExpert/smallestDifference.py
+++ PythonPrep/algoExpert/smallestDifference.py
@@ -1,2 +1,7 @@
 def smallestDifference(arrayOne, arrayTwo):
     print(arrayOne)
+
+arrayOne = [-1, 5, 10, 20, 28, 3]
+arrayTwo = [26, 134, 135, 15, 17]
+
+print(smallestDifference(arrayOne, arrayTwo))
