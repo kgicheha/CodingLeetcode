450bf35986b2aeb963e97eb0cd63eb8890705118
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-18 10:26:14 -0700

    practice

--- PythonPrep/algoExpert/nodeDepths.py
+++ PythonPrep/algoExpert/nodeDepths.py
@@ -6,3 +6,13 @@ class BinaryTree:
 
 def nodeDepths(root):
     print(root)
+
+root = BinaryTree(1)
+root.left = BinaryTree(2)
+root.right = BinaryTree(3)
+root.left.left = BinaryTree(4)
+root.left.right = BinaryTree(5)
+root.right.left = BinaryTree(6)
+root.right.right = BinaryTree(7)
+root.left.left.left = BinaryTree(8)
+root.left.left.right = BinaryTree(9)
