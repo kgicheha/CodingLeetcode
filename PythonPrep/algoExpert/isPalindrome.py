988592e294c392068be70a61e23bb4f9f2921eff
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-17 17:13:07 -0700

    practice

---
+++ PythonPrep/algoExpert/isPalindrome.py
@@ -0,0 +1,19 @@
+'''
+Question Link:
+https://www.algoexpert.io/questions/palindrome-check
+'''
+'''
+Palindrome: a string that's written the same forward and backward
+GIVEN:
+    string
+STEPS:
+    edge case 1:
+
+RESULT:
+    return True if given string is a palindrome
+    else:
+        return False
+'''
+
+def isPalindrome(string):
+    print(string)
