004163165d0c8fcf65f61549bb7d49e305e34af4
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-17 17:25:50 -0700

    practice

--- PythonPrep/algoExpert/isPalindrome.py
+++ PythonPrep/algoExpert/isPalindrome.py
@@ -7,13 +7,43 @@ Palindrome: a string that's written the same forward and backward
 GIVEN:
     string
 STEPS:
-    edge case 1:
+   initilize left pointer to 0
+   initialize right pointer to the last character of the given string
+
+   iterate through given string:
+        if the character in the left pointer is equal to the character in the right pointer:
+            increment left pointer by 1
+            decrement right pointer by 1
+        else:
+            return False
+
+    if it successfully goes through the iteration:
+        return True
 
 RESULT:
     return True if given string is a palindrome
     else:
         return False
+
+
+TIME COMPLEXITY: O(n) || SPACE COMPLEXITY: O(1)
+
 '''
 
 def isPalindrome(string):
-    print(string)
+    lp = 0
+    rp = len(string) - 1
+
+    while lp <= len(string) - 1 and rp >= 0:
+        if string[lp] == string[rp]:
+            lp += 1
+            rp -= 1
+        else:
+            return False
+    return True
+
+
+
+
+string = "abcdcba"
+print(isPalindrome(string))
