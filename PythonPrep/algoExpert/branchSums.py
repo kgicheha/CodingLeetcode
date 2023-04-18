39dc4d9ee9c7e0dde522bdc43aa0991ffd645937
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-18 09:39:56 -0700

    practice

--- PythonPrep/algoExpert/branchSums.py
+++ PythonPrep/algoExpert/branchSums.py
@@ -14,3 +14,15 @@ class BinaryTree:
 def branchSums(root):
     print(root)
 
+
+root = BinaryTree(1)
+root.left = BinaryTree(2)
+root.left.left = BinaryTree(4)
+root.left.right = BinaryTree(5)
+root.left.left.left = BinaryTree(8)
+root.left.left.right = BinaryTree(9)
+root.left.right.left = BinaryTree(10)
+
+root.right = BinaryTree(3)
+root.right.left = BinaryTree(6)
+root.right.right = BinaryTree(7)
