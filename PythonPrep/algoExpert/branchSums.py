a8acc6bf71501e5b324514c1118303469176ba48
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-18 10:21:25 -0700

    practice

--- PythonPrep/algoExpert/branchSums.py
+++ PythonPrep/algoExpert/branchSums.py
@@ -9,10 +9,30 @@ https://www.algoexpert.io/questions/branch-sums
 GIVEN:
     root of a binary tree
 STEPS:
+    initialize empty array to store the sum of each branch
+    initialize currentSum to 0
+
     traverse through each branch
+
+        Base CasE:
+            if node is None:
+                return
+                meaning you have reached the end
+
+        add the value of each node to the currentSum
+
+        if you are at the last leaf node:
+            append the current Sum to the results array
+
+        do this recursively to the left Child
+        then do this recursively to the right child
 
 RESULT:
     return an array of the sum of all the nodes in the binary tree branch
+
+
+
+Time Complexity: O(n) || Space Complexity O(n)
 '''
 
 class BinaryTree:
