a7ddc84205bfaab7dc72ba89ac719c0824239ae7
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-25 09:40:20 -0700

    practice

--- PythonPrep/algoExpert/minimumWaitingTime.py
+++ PythonPrep/algoExpert/minimumWaitingTime.py
@@ -9,26 +9,32 @@ GIVEN:
     ALLOWED TO MUTUTATE THE INPUT ARRAY
 STEPS:
     # sort the given array
-    initialize two pointers
-        left pointer starts at 0
-        right pointer starts at the last element of the array
+
 
     initialize variable that keeps track of the wait time
+
+    traverse sorted array:
+
 RESULTS:
     return the minimum amount of total waiting time for all of the queries
+
+
+Time Complexity -->O(n log n) + n || Space Complexity --> O(1)
 '''
 
 def minimumWaitingTime(queries):
 
     queries.sort()
 
-    min_wait_time = 0
+    totalWaitTime = 0
 
-    for i in range(1,len(queries)):
+    for index, duration in enumerate(queries):
+        queriesLeft = len(queries) - (index + 1)
 
-        min_wait_time += queries[i - 1]
+        totalWaitTime += duration * queriesLeft
 
-    return min_wait_time
+    return totalWaitTime
+
 
 queries = [3, 2, 1, 2, 6]
 print(minimumWaitingTime(queries))
