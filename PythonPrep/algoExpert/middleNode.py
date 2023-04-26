4d844847cc6435027b643f354c7d823b7f89f6eb
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-26 15:10:37 -0700

    practice

--- PythonPrep/algoExpert/middleNode.py
+++ PythonPrep/algoExpert/middleNode.py
@@ -3,7 +3,14 @@ Question Link:
 https://www.algoexpert.io/questions/middle-node
 '''
 
+'''
+GIVEN:
+    a linked list with AT LEAST one node
+STEPS:
+RESULT:
+    return the middle node of the linked list
 
+'''
 class LinkedList:
     def __init__(self, value):
         self.value = value
@@ -12,3 +19,9 @@ class LinkedList:
 def middleNode(linkedList):
     return None
 
+root = LinkedList(2)
+root.next = LinkedList(7)
+root.next.next = LinkedList(3)
+root.next.next.next = LinkedList(5)
+
+print(middleNode(root))
