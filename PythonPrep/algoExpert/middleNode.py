2970aff7737a16aa966d313397fa209b7879faa4
Kevin Gicheha <gichehakevin@gmail.com>
2023-04-26 15:04:57 -0700

    practice

---
+++ PythonPrep/algoExpert/middleNode.py
@@ -0,0 +1,14 @@
+'''
+Question Link:
+https://www.algoexpert.io/questions/middle-node
+'''
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def middleNode(linkedList):
+    return None
+
