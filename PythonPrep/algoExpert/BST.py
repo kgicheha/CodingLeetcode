cc9d0ccef6c6417a8e04af043e29e78c64889536
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-01 10:54:19 -0700

    practice

--- PythonPrep/algoExpert/BST.py
+++ PythonPrep/algoExpert/BST.py
@@ -18,15 +18,13 @@ class BST:
 
             if value < currentNode:
                 if currentNode.left is None:
-                    currentNode = BST(value)
-
+                    currentNode.left = BST(value)
                 else:
                     currentNode = currentNode.left
 
-
             if value > currentNode:
                 if currentNode.right is None:
-                    currentNode = BST(value)
+                    currentNode.right = BST(value)
                 else:
                     currentNode = currentNode.right
         return self
@@ -44,7 +42,6 @@ root.insert(2)
 
 '''
 INSERTION USING ITERATION
-
     initialize currentNode to self
 
     while True:
@@ -53,5 +50,23 @@ INSERTION USING ITERATION
             check to see if the left child is empty
 
             if the left child is empty:
+                initialize the left child to the BST function with the given value as an argument
 
+            else if the a left child does exist:
+                traverse to the left child by setting the currentNode equal to the currentNode.left child
+
+        else:
+            check to see if right child is empty
+
+            if the right child is empty:
+                set the child equal to the value using the BST function and passing the value as its argument
+
+            else if a right child does exist:
+                move on to the right child then check the condition
+                do this by setting the currentNode equal to its right child
+
+    return self
+
+Average Time Complexity --> O(log n) || Space Complexity --> O(1)
+Worst Time Complexity --> O(n) || Space Complexity --> O(1)
 '''
