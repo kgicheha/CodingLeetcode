ef73b5674e18d7d08999036c873c87f037c5b1f0
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-09 09:52:52 -0700

    practice

--- PythonPrep/algoExpert/validBST.py
+++ PythonPrep/algoExpert/validBST.py
@@ -5,8 +5,12 @@ https://www.algoexpert.io/questions/validate-bst
 
 '''
 GIVEN:
+    a tree
 STEPS:
+    traverse given tree if
 RESULT:
+    True if the given tree is valid
+    False if the given tree is invalid
 '''
 class BST:
 
@@ -16,4 +20,6 @@ class BST:
         self.right = None
 
     def validateBst(tree):
+
+
         pass
