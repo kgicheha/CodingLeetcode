726e5a451a71a5301f0c432e137f38d259e9ee87
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-17 12:06:38 -0700

    practice

--- PythonPrep/algoExpert/getNthFib.py
+++ PythonPrep/algoExpert/getNthFib.py
@@ -4,7 +4,12 @@ https://www.algoexpert.io/questions/nth-fibonacci
 '''
 
 def getNthFib(n):
-    pass
+    if n == 2:
+        return 1
+    elif n == 1:
+        return 0
+    else:
+        return getNthFib(n - 1) + getNthFib(n - 2)
 
 n = 9
 print(getNthFib(n))
