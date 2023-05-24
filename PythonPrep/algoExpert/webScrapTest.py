5533a46ba198f4c0fc60ce39662d54a05129cf81
Kevin Gicheha <gichehakevin@gmail.com>
2023-05-24 09:43:11 -0700

    practice

--- PythonPrep/algoExpert/webScrapTest.py
+++ PythonPrep/algoExpert/webScrapTest.py
@@ -1,1 +1,12 @@
 # https://finance.yahoo.com/quote/AAPL?p=AAPL
+
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+# url = 'https://finance.yahoo.com/quote/AAPL?p=AAPL'
+url = 'https://finance.yahoo.com/quote/AAPL'
+response = requests.get(url, headers=headers)
+print(response.status_code)
+
