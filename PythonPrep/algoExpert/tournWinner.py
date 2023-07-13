7f42a538423a9c5e98c18a9286845b1fb2b24e2b
Kevin Gicheha <gichehakevin@gmail.com>
2023-07-13 10:16:42 -0700

    algo expert

--- PythonPrep/algoExpert/tournWinner.py
+++ PythonPrep/algoExpert/tournWinner.py
@@ -5,14 +5,15 @@ https://www.algoexpert.io/questions/tournament-winner
 '''
 '''
 GIVEN:
-
+    an array of pairs representing the teams that competed against each other
+    an array containing the results of each competition
 STEPS:
 
 RESULT:
-
+    returns the winner of the tournament
 '''
 
 
 
-def tournWinner():
+def tournWinner(competitions, results):
     print("yes")
