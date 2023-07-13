615d1d00802044244b2fd64c5563a65ceaa885b0
Kevin Gicheha <gichehakevin@gmail.com>
2023-07-13 10:19:45 -0700

    algo expert

--- PythonPrep/algoExpert/tournWinner.py
+++ PythonPrep/algoExpert/tournWinner.py
@@ -6,7 +6,12 @@ https://www.algoexpert.io/questions/tournament-winner
 '''
 GIVEN:
     an array of pairs representing the teams that competed against each other
+        competitions = [homeTeam, awayTeam]
+            represented as strings
     an array containing the results of each competition
+        0 means the awayTeam won
+        1 means the homeTeam won
+
 STEPS:
 
 RESULT:
