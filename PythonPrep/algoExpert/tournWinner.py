984bccbf19b51947941b2608f95f045e18367ee9
Kevin Gicheha <gichehakevin@gmail.com>
2023-07-13 10:25:16 -0700

    algo expert

--- PythonPrep/algoExpert/tournWinner.py
+++ PythonPrep/algoExpert/tournWinner.py
@@ -13,11 +13,32 @@ GIVEN:
         1 means the homeTeam won
 
 STEPS:
+    create empty dictionary that tracks the score
+
+    loop through given competitions array
+        in the current competition:
+            if the result is 0:
+                if the awayTeam is in the score dictionary:
+                    increments its score by 1
+                else:
+                    add the awayTeam in the scores dictionary by setting the score to 1
+            if the result is 1:
+                if the home team is already present in the score dictinary:
+                    increment their score by 1
+                else:
+                    add the homeTeam in the scores dictionary by setting the score to 1
+
+
+    return the team with the highest max number in the dicitionary
 
 RESULT:
     returns the winner of the tournament
 '''
 
+'''
+EXAMPLE:
+
+'''
 
 
 def tournWinner(competitions, results):
