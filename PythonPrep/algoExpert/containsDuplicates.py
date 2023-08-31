d466314243c6147fbd8a6bb14e621afa8fe6cdae
Kevin Gicheha <gichehakevin@gmail.com>
2023-08-31 09:30:50 -0700

    leetcode

--- PythonPrep/algoExpert/containsDuplicates.py
+++ PythonPrep/algoExpert/containsDuplicates.py
@@ -1,3 +1,16 @@
-def containsDuplicates():
+'''
+Link: https://leetcode.com/problems/contains-duplicate/
+
+'''
+'''
+GIVEN:
+    array of numbers
+STEPS:
+RESULT:
+    return True, if any values appears AT LEAST TWICE in the array
+    else return False if every element is DISTINCT
+
+'''
+def containsDuplicates(nums):
     return True
 
