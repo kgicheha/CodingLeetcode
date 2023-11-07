021ead2533cb1bcab396064748104a67863fa90a
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-07 11:42:33 -0800

    practice question

---
+++ PythonPrep/newAlgoExpert/threeNumberSum.py
@@ -0,0 +1,13 @@
+def threeNumSum(array, targetSum):
+    pass
+
+'''Link: https://www.algoexpert.io/questions/three-number-sum '''
+
+'''
+GIVEN:
+STEPS:
+RESULT:
+'''
+
+
+
--- PythonPrep/newAlgoExpert/twoNumberSum.py
+++ PythonPrep/newAlgoExpert/twoNumberSum.py
@@ -23,6 +23,33 @@ STEPS:
 
         if you traverse the entire array and no matches are found:
             return an empty array
+
+
+    STRATEGY 2: 2 pointer solution --> TIME COMPLEXITY: O (nlogn), SPACE COMPLEXITY: O(1)
+
+        sort given array
+
+        intialize left pointer to 0
+        initialize right pointer to last index of the given array
+
+
+        iterate through given array
+
+            add the number at the left pointer and the number at the right pointer
+
+            if the sum of both are > given target:
+                move the right pointer to the left by 1
+
+            else if the sum of both are < given target:
+                move the left pointer to the right by 1
+
+            else:
+                return [left pointer's value, right pointer's value]
+
+
+        return []
+
+
 RESULT:
     return array
 
@@ -31,16 +58,37 @@ RESULT:
 
 
 def twoNumberSum(array, targetSum):
-    difference = {}
+    # STRATEGY 1:
+    # difference = {}
 
-    for num in array:
-        cur_difference = targetSum - num
+    # for num in array:
+    #     cur_difference = targetSum - num
 
-        if cur_difference in difference:
-            return [num, cur_difference]
+    #     if cur_difference in difference:
+    #         return [num, cur_difference]
 
+    #     else:
+    #         difference[num] = cur_difference
+
+    # return []
+
+
+    # STRATEGY 2:
+    array.sort()
+
+    lp = 0
+    rp = len(array) - 1
+
+
+    while lp < rp:
+        current_sum = array[lp] + array[rp]
+
+        if current_sum > targetSum:
+            rp -=1
+        elif current_sum < targetSum:
+            lp += 1
         else:
-            difference[num] = cur_difference
+            return [array[lp], array[rp]]
 
     return []
 
