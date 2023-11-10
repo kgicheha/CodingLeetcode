973c5f4e01a716989bb06f836a588d8e93c1982a
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-09 19:29:44 -0800

    neetCode practice

--- PythonPrep/neetCode/arraysAndHashing/groupAnagrams.py
+++ PythonPrep/neetCode/arraysAndHashing/groupAnagrams.py
@@ -1,6 +1,34 @@
-def groupAnagrams(strs):
-    pass
+from collections import defaultdict
 
+def groupAnagrams(strs):
+    # STRATEGY 1:
+
+    result = defaultdict(list)
+
+    for s in strs:
+        print(sorted(s))
+        sortedChar = "".join(sorted(s))
+
+        result[sortedChar].append(s)
+
+    return result
+
+    # STRATEGY 2
+    # result = defaultdict(list)
+
+    # for s in strs:
+    #     count = [0] * 26
+
+    #     for char in s:
+    #         count[ord(char) - ord("a")] += 1
+
+    #     result[tuple(count)].append(s)
+
+
+    # return result.values()
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams(strs))
 '''
 Link:
 https://leetcode.com/problems/group-anagrams/
@@ -8,5 +36,27 @@ https://leetcode.com/problems/group-anagrams/
 '''
 
 '''
+An Anagram is a word or phrase formed by rearranging
+the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
 GIVEN: array of strings
+STEPS:
+
+    # STRATEGY 1: sorting the characters --> TIME COMPL = O(m*n + nlogn) , SPACE COMPL  = O(n)
+
+        import defaultdict from collections
+
+        create empty dictionary
+
+        iterate through given array:
+            for each array:
+                sort the character
+                join the character
+                add the  sortedCharacter to the dictionary with the unsorted version as its value
+
+        return the result dictionary
+
+RESULTS:
+
 '''
