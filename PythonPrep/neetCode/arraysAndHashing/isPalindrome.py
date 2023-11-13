df105130c56847d071c5151ae0b1b4e174e06164
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-12 19:09:29 -0800

    neetCode practice

---
+++ PythonPrep/neetCode/arraysAndHashing/isPalindrome.py
@@ -0,0 +1,20 @@
+def isPalindrome(s):
+    pass
+
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
+
+'''
+Link: https://leetcode.com/problems/valid-palindrome/
+'''
+
+'''
+A phrase is a palindrome if, after converting all uppercase letters
+into lowercase letters and removing all non-alphanumeric characters,
+ it reads the same forward and backward.
+
+GIVEN:
+    a string
+STEPS:
+RESULTS:
+'''
