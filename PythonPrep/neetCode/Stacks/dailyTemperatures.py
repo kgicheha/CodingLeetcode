65c7d229d3616e4f3abaf71302769cd7d8d26eba
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-14 14:30:40 -0800

    stack practice

---
+++ PythonPrep/neetCode/Stacks/dailyTemperatures.py
@@ -0,0 +1,15 @@
+def dailyTemperatures(temperatures):
+    pass
+
+temperatures = [73,74,75,71,69,72,76,73]
+# Output: [1,1,4,2,1,1,0,0]
+print(dailyTemperatures(temperatures))
+'''
+Link: https://leetcode.com/problems/daily-temperatures/
+'''
+
+'''
+GIVEN:
+STEPS:
+RESULTS:
+'''
