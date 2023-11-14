743508fc5d2efd3cba81cfc67ac8b324e89199f1
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-14 10:22:05 -0800

    stack practice questions

---
+++ PythonPrep/neetCode/Stacks/generateParenthesis.py
@@ -0,0 +1,17 @@
+def generateParenthesis(n):
+    pass
+
+n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+print(generateParenthesis(n))
+
+'''
+Link: https://leetcode.com/problems/generate-parentheses/
+'''
+'''
+GIVEN:
+STEPS:
+RESULTS:
+
+
+'''
