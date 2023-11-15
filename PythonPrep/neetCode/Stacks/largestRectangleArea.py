755e6d018c6265b2e42d78aa7081114171949613
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-14 23:16:24 -0800

    stack practice

---
+++ PythonPrep/neetCode/Stacks/largestRectangleArea.py
@@ -0,0 +1,17 @@
+def largestRectangleArea(heights):
+    pass
+
+Input: heights = [2,1,5,6,2,3]
+# Output: 10
+print(largestRectangleArea(heights))
+
+'''
+Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
+'''
+
+'''
+GIVEN:
+STEPS:
+RESULTS:
+
+'''
