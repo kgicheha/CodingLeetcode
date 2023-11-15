3d75952bc15abdaa33cc5ba5e5b7f5d70aadfd37
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-15 13:45:14 -0800

    stack practice

--- PythonPrep/neetCode/binarySearch/search.py
+++ PythonPrep/neetCode/binarySearch/search.py
@@ -3,9 +3,9 @@ def search(nums, target):
     lp = 0
     rp = len(nums) - 1
 
-    while lp < rp:
+    while lp <= rp:
 
-        mid = (left + right) // 2
+        mid = (lp + rp) // 2
 
         if nums[mid] == target:
             return mid
@@ -18,7 +18,7 @@ def search(nums, target):
 
     return -1
 
-Input: nums = [-1,0,3,5,9,12]
+nums = [-1,0,3,5,9,12]
 target = 9
 # Output: 4
 print(search(nums, target))
@@ -36,15 +36,17 @@ STEPS:
 
     initialize left and right pointer equals to 0 and len(nums)-1, respectively
 
-    while left < right:
+    while left <= right:
         mid = (left + right) // 2
 
         if nums[mid] == target:
             return mid
         elif nums[mid] < target:
-
-            left = mid + 1
+            # 3 < 9
+            move the left pointer up
+                left = mid + 1
         else:
+            move the right pointer down
             right = mid - 1
     return -1
 RESULTS:
---
+++ PythonPrep/neetCode/binarySearch/searchMatrix.py
@@ -0,0 +1,17 @@
+def searchMatrix():
+    pass
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 13
+# Output: false
+print(searchMatrix(matrix, target))
+
+'''
+Link: https://leetcode.com/problems/search-a-2d-matrix/
+
+'''
+'''
+GIVEN:
+STEPS:
+RESULT:
+'''
