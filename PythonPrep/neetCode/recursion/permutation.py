863b7d65c05da4fa083f2625b91dd46999b173cb
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-20 20:19:13 -0800

    recursion practice

--- PythonPrep/neetCode/recursion/permutation.py
+++ PythonPrep/neetCode/recursion/permutation.py
@@ -1,5 +1,9 @@
 def permutions(array):
-    pass
+
+    if len(array) == 0:
+        return []
+
+
 
 array = [1,2,3]
 
@@ -10,4 +14,8 @@ GIVEN:
     array of unique integers
 STEPS:
 RESULTS:
+    return all the permutations of those integers
+
+    if input is empty,
+        return an empty array
 '''
