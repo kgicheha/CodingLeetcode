502e9194651bbc69317dabd584d1b66b2591fc43
Kevin Gicheha <gichehakevin@gmail.com>
2023-11-21 06:59:37 -0800

    recursion practice

--- PythonPrep/neetCode/recursion/permutation.py
+++ PythonPrep/neetCode/recursion/permutation.py
@@ -1,8 +1,20 @@
 def permutions(array):
-
     if len(array) == 0:
         return []
 
+    # base case
+    if len(array) == 1:
+        return [array[:]]
+
+    for i in range(len(array)):
+        num = array.pop(0)
+
+        perms = self.permutations(array)
+
+        for perm in perms:
+            perm.append(num)
+            result.append(perm)
+        array.append(num)
 
 
 array = [1,2,3]
